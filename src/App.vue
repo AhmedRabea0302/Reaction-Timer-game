4fffa188ff952206135ddac3780448e25971d823
AhmedRabea0302 <arabea169@gmail.com>
2021-12-20 19:25:38 +0200

    Adding Block Component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,37 @@
 <template>
   <h1>Timer Reaction Game</h1>
+  <button @click="start">Play</button>
+
+  <div v-if="isPlaying">
+    <Block :delay="delay" />
+  </div>
 </template>
 
 <script>
+import Block from './components/Block.vue'
+import Results from './components/Results.vue'
 
 export default {
   name: 'App',
+  data() {
+    return {
+      isPlaying: false,
+      delay: null
+    }
+  },
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 5000;
+      setTimeout(() => this.isPlaying = true, this.delay)
+
+      console.log(this.delay);
+    }
+  },
   components: {
-  }
+    Block,
+    Results,
+  },
+
 }
 </script>
 
